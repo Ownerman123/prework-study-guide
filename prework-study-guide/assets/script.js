32fd344b189cda86273f5e233ed0adeaa20a02d9
Ownerman123 <charlesshumway1@gmail.com>
2024-02-06 15:15:10 -0700

    added more pointless but fun JavaScript

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,22 +1,36 @@
 const topics = ['HTML' , 'CSS', 'Git' , 'JavaScript' ];
 
-   
+console.log('Here are the topics we learned through Prework:');
+ListTopics();
+
+console.log('Which topic should we study first?');
+SelectTopic();
+
+function ListTopics() {   
 
     for(let x = 0; x < topics.length; x++) {
 
-        if (topics[x] === 'HTML') {
-            console.log("Let's study HTML!");
-          } else if (topics[x] === 'CSS') {
-            console.log("Let's study CSS!");
-          } else if (topics[x] === 'Git') {
-            console.log("Let's study Git!");
-          } else if (topics[x] === 'JavaScript') {
-            console.log("Let's study JavaScript!");
-          } else {
-            console.log('Please try again!');
-          }
+        console.log(topics[x]);
+        
 
     }
+}
+
+function SelectTopic() {
+
+    let topic = topics[Math.floor(Math.random() * topics.length)];
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
 
 
-
+}
